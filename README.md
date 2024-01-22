236fcdd1b32bfd00f5d9cffc63816f432b47179c
Will Gantt <wgantt.iv@gmail.com>
2024-01-22 13:55:07 -0500

    initial release of data, predictions, and evaluation scripts

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # MultiMUC
-Official repository for the MultiMUC dataset
+
+This is the official repository for the MultiMUC dataset, as presented in our EACL 2024 paper, *MultiMUC: Multilingual Template Filling on MUC-4*.
+
+# Getting Started
+
+The repository has the following structure:
+
+- `data.zip`: The MultiMUC data files
+- `predictions.zip`: (corrected) test set predictions for IterX, GTT, and ChatGPT
+- `iterx/`: IterX source code (needed for computing CEAF-RME scores
+- `scripts/eval.py`: Evaluation script for computing CEAF-REE scores
+
+If you wish only to use the data and/or model predictions, and do not care about running evaluation, then no setup is required (beyond unzipping `data.zip` and `predictions.zip`). However, if you wish to run evaluation on the model predictions, see [Evaluation](#evaluation) below. Beyond the data, predictions, and evaluation scripts provided above, we aim to release instructions for model training and inference soon. Please create an issue if you need this functionality and you find that it has not yet been provided.
+
+## Evaluation
+
+Our paper reports two template filling evaluation metrics, CEAF-REE and CEAF-RME (please see the paper for details on these and on the differences between them). Note that we provide printouts of the scoring scripts for both metrics in the same directory as the test set predictions for each (`model`, `setting`, `language`) triple. These can be found in the `predictions/{model}/{setting}/{language}/eval_{model}_ceaf_{rme|ree}.out` files. However, if you wish to replicate these results yourself on our model predictions (or for your own predictions, *mutatis mutandis*), the instructions are given below.
+
+To compute CEAF-REE scores, you should use the provided `scripts/eval.py` script. This should require only a Python installation (we used 3.9) to run. It can be run from this directory as follows:
+
+```
+python scripts.eval.py --pred_file predictions/{model}/{setting}/{language}/test_preds.json --gold_file data/corrected/{language}/test.jsonl [--chinese]
+```
+
+where `model` is one of "iterx", "gtt", or "chatgpt"; `setting` is one of "bi", "tgt\_auto", or "tgt\_man"; and `language` is one of "ar", "fa", "ko", "ru", or "zh". The `--chinese` flag should be set only if evaluating on Chinese (`zh`).
+
+If you wish to compute *CEAF-RME* scores, you will have to initialize the `iterx` git submodule as follows:
+
+```
+cd iterx
+git submodule init
+git submodule update
+```
+
+Next, you should follow the environment configuration instructions for the IterX repo that are provided [here](https://github.com/wanmok/iterx#environment-setup). Once you have done so, `cd` into `iterx`, then compute CEAF-RME scores as follows:
+
+```
+PYTHONPATH=./src python scripts/ceaf-scorer.py score --dataset MUC ../../predictions/{model}/{setting}/{language}/test\_preds.json ../../data/corrected/{language}/test.jsonl --file-type GTT 
+```
+
+where the possible values for `model`, `setting`, and `language` are the same as above.
---
+++ scripts/eval.py
@@ -0,0 +1,413 @@
+"""A lightly adapted version of the following CEAF-REE scoring script:
+
+https://github.com/xinyadu/gtt/blob/master/eval.py
+
+An effort was made to keep this script as close to the original as possible,
+which means that it will run *very* slowly in cases where the predictions
+contain many predicted templates for a particular example. This is because
+the original script uses a brute-force approach to finding the best mapping
+between predicted and gold templates. For all model predictions included in
+this repo, the script finishes in at most a few minutes, but more typically
+within seconds.
+"""
+import re
+import string
+import json
+import argparse
+from collections import OrderedDict
+import itertools
+
+tag2role = OrderedDict(
+    {
+        "incident_type": "incident_type",
+        "perp_individual_id": "PerpInd",
+        "perp_organization_id": "PerpOrg",
+        "phys_tgt_id": "Target",
+        "hum_tgt_name": "Victim",
+        "incident_instrument_id": "Weapon",
+    }
+)
+
+
+def normalize_string(s, is_chinese=False):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+
+    def remove_articles(text):
+        regex = re.compile(r"\b(a|an|the)\b", re.UNICODE)
+        return re.sub(regex, " ", text)
+
+    def white_space_fix(text):
+        return " ".join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return "".join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    def normalize_chinese(text):
+        return "".join(text.split())
+
+    ret = white_space_fix(remove_articles(remove_punc(lower(s))))
+    if is_chinese:
+        ret = normalize_chinese(ret)
+    return ret
+
+
+def f1(p_num, p_den, r_num, r_den, beta=1):
+    p = 0 if p_den == 0 else p_num / float(p_den)
+    r = 0 if r_den == 0 else r_num / float(r_den)
+    return 0 if p + r == 0 else (1 + beta * beta) * p * r / (beta * beta * p + r)
+
+
+def matching(c1, c2):
+    # similarity: if c2 (pred) is subset of c1 (gold) return 1
+    for m in c2:
+        if m not in c1:
+            return 0
+    return 1
+
+
+def is_valid_mapping(mapping):
+    reverse_mapping = {}
+    for k in mapping:
+        v = mapping[k]
+        if v not in reverse_mapping:
+            reverse_mapping[v] = [k]
+        else:
+            reverse_mapping[v].append(k)
+
+    for v in reverse_mapping:
+        if v == -1:
+            continue
+        if len(reverse_mapping[v]) > 1:
+            return False
+
+    return True
+
+
+def is_valid_mention(mention, doctext):
+    mention_str, mention_offset = mention
+    if not mention_str or not mention_offset:
+        return False
+    return doctext[mention_offset : mention_offset + len(mention_str)] == mention_str
+
+
+def score(mapping, pred, gold):
+    ex_result = OrderedDict()
+    all_keys = list(role for _, role in tag2role.items()) + ["micro_avg"]
+    for key in all_keys:
+        ex_result[key] = {
+            "p_num": 0,
+            "p_den": 0,
+            "r_num": 0,
+            "r_den": 0,
+            "p": 0,
+            "r": 0,
+            "f1": 0,
+        }
+    # if invalid mapping, return 0
+    # if not is_valid_mapping(mapping):
+    #     return ex_result
+
+    mapped_temp_pred = []
+    mapped_temp_gold = []
+    for pred_temp_idx in mapping:
+        gold_temp_idx = mapping[pred_temp_idx]
+        if type(pred[pred_temp_idx]["incident_type"]) != str:
+            pred[pred_temp_idx]["incident_type"] = "attack"
+        if (
+            gold_temp_idx != -1
+            and pred[pred_temp_idx]["incident_type"]
+            in gold[gold_temp_idx]["incident_type"]
+        ):  # attach vs attach / bombing
+            mapped_temp_pred.append(pred_temp_idx)
+            mapped_temp_gold.append(gold_temp_idx)
+            pred_temp, gold_temp = pred[pred_temp_idx], gold[gold_temp_idx]
+
+            # prec
+            for role in pred_temp.keys():
+                if role == "incident_type":
+                    ex_result[role]["p_den"] += 1
+                    ex_result[role]["p_num"] += 1
+                    continue
+                for entity_pred in pred_temp[role]:
+                    ex_result[role]["p_den"] += 1
+                    correct = False
+                    for entity_gold in gold_temp[role]:
+                        # import ipdb; ipdb.set_trace()
+                        if matching(entity_gold, entity_pred):
+                            correct = True
+                    if correct:
+                        ex_result[role]["p_num"] += 1
+
+            # recall
+            for role in gold_temp.keys():
+                if role == "incident_type":
+                    ex_result[role]["r_den"] += 1
+                    ex_result[role]["r_num"] += 1
+                    continue
+                for entity_gold in gold_temp[role]:
+                    ex_result[role]["r_den"] += 1
+                    correct = False
+                    for entity_pred in pred_temp[role]:
+                        if matching(entity_gold, entity_pred):
+                            correct = True
+                    if correct:
+                        ex_result[role]["r_num"] += 1
+
+    # spurious
+    for pred_temp_idx in range(len(pred)):
+        pred_temp = pred[pred_temp_idx]
+        if pred_temp_idx not in mapped_temp_pred:
+            for role in pred_temp:
+                if role == "incident_type":
+                    ex_result[role]["p_den"] += 1
+                    continue
+                for entity_pred in pred_temp[role]:
+                    ex_result[role]["p_den"] += 1
+
+    # missing
+    for gold_temp_idx in range(len(gold)):
+        gold_temp = gold[gold_temp_idx]
+        if gold_temp_idx not in mapped_temp_gold:
+            for role in gold_temp:
+                if role == "incident_type":
+                    ex_result[role]["r_den"] += 1
+                    continue
+                for entity_gold in gold_temp[role]:
+                    ex_result[role]["r_den"] += 1
+
+    ex_result["micro_avg"]["p_num"] = sum(
+        ex_result[role]["p_num"] for _, role in tag2role.items()
+    )
+    ex_result["micro_avg"]["p_den"] = sum(
+        ex_result[role]["p_den"] for _, role in tag2role.items()
+    )
+    ex_result["micro_avg"]["r_num"] = sum(
+        ex_result[role]["r_num"] for _, role in tag2role.items()
+    )
+    ex_result["micro_avg"]["r_den"] = sum(
+        ex_result[role]["r_den"] for _, role in tag2role.items()
+    )
+
+    for key in all_keys:
+        ex_result[key]["p"] = (
+            0
+            if ex_result[key]["p_num"] == 0
+            else ex_result[key]["p_num"] / float(ex_result[key]["p_den"])
+        )
+        ex_result[key]["r"] = (
+            0
+            if ex_result[key]["r_num"] == 0
+            else ex_result[key]["r_num"] / float(ex_result[key]["r_den"])
+        )
+        ex_result[key]["f1"] = f1(
+            ex_result[key]["p_num"],
+            ex_result[key]["p_den"],
+            ex_result[key]["r_num"],
+            ex_result[key]["r_den"],
+        )
+
+    return ex_result
+
+
+def eval_tf(preds, golds, docids=[], is_chinese=False):
+    # normalization mention strings
+    for docid in preds:
+        for idx_temp in range(len(preds[docid])):
+            for role in preds[docid][idx_temp]:
+                if role == "incident_type":
+                    continue
+                for idx in range(len(preds[docid][idx_temp][role])):
+                    for idy in range(len(preds[docid][idx_temp][role][idx])):
+                        preds[docid][idx_temp][role][idx][idy] = normalize_string(
+                            preds[docid][idx_temp][role][idx][idy], is_chinese
+                        )
+    for docid in golds:
+        for idx_temp in range(len(golds[docid])):
+            for role in golds[docid][idx_temp]:
+                if role == "incident_type":
+                    continue
+                for idx in range(len(golds[docid][idx_temp][role])):
+                    for idy in range(len(golds[docid][idx_temp][role][idx])):
+                        golds[docid][idx_temp][role][idx][idy] = normalize_string(
+                            golds[docid][idx_temp][role][idx][idy], is_chinese
+                        )
+
+    # get eval results
+    result = OrderedDict()
+    all_keys = list(role for _, role in tag2role.items()) + ["micro_avg"]
+    for key in all_keys:
+        result[key] = {
+            "p_num": 0,
+            "p_den": 0,
+            "r_num": 0,
+            "r_den": 0,
+            "p": 0,
+            "r": 0,
+            "f1": 0,
+        }
+
+    if not docids:
+        for docid in golds:
+            docids.append(docid)
+
+    for docid in docids:
+        pred = preds[docid]
+        gold = golds[docid]
+        K, V = list(range(len(pred))), list(range(len(gold)))
+        V = V + [-1]
+        init_maps = [dict(zip(K, p)) for p in itertools.product(V, repeat=len(K))]
+
+        ex_best = None
+        map_best = None
+        for mapping in init_maps:
+            if not is_valid_mapping(mapping):
+                continue
+            ex_result = score(mapping, pred, gold)
+            if ex_best is None:
+                ex_best = ex_result
+                map_best = mapping
+            elif ex_result["micro_avg"]["f1"] > ex_best["micro_avg"]["f1"]:
+                ex_best = ex_result
+                map_best = mapping
+
+        # sum for one docid
+        for role in all_keys:
+            if role == "micro_avg":
+                continue
+            result[role]["p_num"] += ex_best[role]["p_num"]
+            result[role]["p_den"] += ex_best[role]["p_den"]
+            result[role]["r_num"] += ex_best[role]["r_num"]
+            result[role]["r_den"] += ex_best[role]["r_den"]
+
+    # micro average
+    result["micro_avg"]["p_num"] = sum(
+        result[role]["p_num"] for _, role in tag2role.items()
+    )
+    result["micro_avg"]["p_den"] = sum(
+        result[role]["p_den"] for _, role in tag2role.items()
+    )
+    result["micro_avg"]["r_num"] = sum(
+        result[role]["r_num"] for _, role in tag2role.items()
+    )
+    result["micro_avg"]["r_den"] = sum(
+        result[role]["r_den"] for _, role in tag2role.items()
+    )
+
+    for key in all_keys:
+        result[key]["p"] = (
+            0
+            if result[key]["p_num"] == 0
+            else result[key]["p_num"] / float(result[key]["p_den"])
+        )
+        result[key]["r"] = (
+            0
+            if result[key]["r_num"] == 0
+            else result[key]["r_num"] / float(result[key]["r_den"])
+        )
+        result[key]["f1"] = f1(
+            result[key]["p_num"],
+            result[key]["p_den"],
+            result[key]["r_num"],
+            result[key]["r_den"],
+        )
+
+    return result
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--pred_file", default=None, type=str, required=False, help="preds output file"
+    )
+    parser.add_argument(
+        "--gold_file",
+        default="./data/muc/processed/test.json",
+        type=str,
+        required=False,
+        help="gold file",
+    )
+    parser.add_argument(
+        "--chinese",
+        default=False,
+        action="store_true",
+        help="set this flag if scoring Chinese data to normalize mentions",
+    )
+    args = parser.parse_args()
+
+    ## get pred and gold extracts
+    preds = OrderedDict()
+    golds = OrderedDict()
+    with open(args.pred_file, encoding="utf-8") as f:
+        out_dict = json.load(f)
+        for docid in out_dict:
+            preds[docid] = out_dict[docid]["pred_templates"]
+
+    bad_gold_mentions = 0
+    total_gold_mentions = 0
+    with open(args.gold_file, encoding="utf-8") as f:
+        for line in f:
+            line = json.loads(line)
+            docid = str(
+                int(line["docid"].split("-")[0][-1]) * 10000
+                + int(line["docid"].split("-")[-1])
+            )
+            templates_raw = line["templates"]
+            templates = []
+            for template_raw in templates_raw:
+                template = OrderedDict()
+                for role, value in template_raw.items():
+                    if role == "incident_type":
+                        template[role] = value
+                    else:
+                        template[role] = []
+                        for entity_raw in value:
+                            entity = []
+                            for mention_offset_pair in entity_raw:
+                                # This will drop any mention annotated in the gold data
+                                # that does not also appear in the document text. This may
+                                # happen because of (e.g.) poor translations.
+                                if is_valid_mention(
+                                    mention_offset_pair, line["doctext"]
+                                ):
+                                    entity.append(mention_offset_pair[0])
+                                else:
+                                    bad_gold_mentions += 1
+                                total_gold_mentions += 1
+                            if entity:
+                                template[role].append(entity)
+                if template not in templates:
+                    templates.append(template)
+            golds[docid] = templates
+    # we'd like to know what fraction of gold mentions are dropped
+    # due to not appearing in the document text
+    print("bad gold mentions: {}/{}".format(bad_gold_mentions, total_gold_mentions))
+
+    all_keys = list(role for _, role in tag2role.items()) + ["micro_avg"]
+    docids = []
+    results = eval_tf(preds, golds, docids, args.chinese)
+    str_print = []
+    for key in all_keys:
+        if key == "micro_avg":
+            print("***************** {} *****************".format(key))
+        else:
+            print("================= {} =================".format(key))
+
+        str_print += [
+            results[key]["p"] * 100,
+            results[key]["r"] * 100,
+            results[key]["f1"] * 100,
+        ]
+        print(
+            "P: {:.2f}%,  R: {:.2f}%, F1: {:.2f}%".format(
+                results[key]["p"] * 100,
+                results[key]["r"] * 100,
+                results[key]["f1"] * 100,
+            )
+        )  # phi_strict
+    str_print = ["{:.2f}".format(r) for r in str_print]
+    print("print: {}".format(" ".join(str_print)))
+    print()
